2b5f325ed2248683c4341b57525a98b3524b49e1
Eduardo Parra <edue17@gmail.com>
2015-11-08 20:48:18 +0100

    Update sync_files.py

--- sync_files.py
+++ sync_files.py
@@ -18,7 +18,7 @@
 ##    You should have received a copy of the GNU General Public License		##
 ##    along with this program.  If not, see <http://www.gnu.org/licenses/>. 	##
 ##										##
-##   MegaUp version 0.1, Copyright (C) 2015 Eduardo Parra Mazuecos		##
+##   Version 0.1, Copyright (C) 2015 Eduardo Parra Mazuecos			##
 ##   email: eduparra90@gmail.com		                  		##
 ##   Website: http://www.eduardoparra.es 					##
 ##										##
