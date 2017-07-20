aef32f2affd72057ec4599153caabe966487de16
Eduardo Parra <edue17@gmail.com>
2017-07-21 01:33:42 +0200

    Update trakt.js

--- trakt.js
+++ trakt.js
@@ -7,19 +7,43 @@
 // @grant       none
 // ==/UserScript==
 //Titulo
-if(document.title = "Dashboard - Trakt.tv") { document.title = "Inicio - Trakt.tv"; }
+
 
 //Cabecera
 $('#main-nav ul:first').html('<li><a href="/shows/trending">TV</a></li><li><a href="/movies/trending">Peliculas</a></li><li><a href="/calendars">Calendario</a></li>')
 $('#main-nav ul:last').html('<li><a href="/discover">Descubrir</a></li><li><a href="/apps">Aplicaciones</a></li><li><a href="/vip">VIP</a></li>')
 
-//Dashboard
-var text = $('#results-top-wrapper div:nth-child(2) div:first H1').html().replace('Hello','Hola')
-$('#results-top-wrapper div:nth-child(2) div:first H1').html(text)
-var text = $('#results-top-wrapper div:nth-child(2) div:first h2').html().replace('Member since','Miembro desde')
-$('#results-top-wrapper div:nth-child(2) div:first h2').html(text)
-$('#results-top-wrapper div:nth-child(2) div:nth-child(3)').html('Año en<br>Datos')
-$('#results-top-wrapper div:nth-child(2) a:nth-child(2) div:first div:first').html('Tu<br>Perfil')
+$('#user-menu ul').html('<li><a href="/dashboard">Inicio</a></li><li><a href="/users/soker">Perfil</a></li><li><a href="/users/soker/history">Historial</a></li><li><a href="/users/soker/progress">Progreso</a></li><li><a href="/users/soker/collection">Colección</a></li><li><a href="/users/soker/ratings">Calificaciones</a></li><li><a href="/users/soker/lists">Listas</a></li><li><a href="/users/soker/comments">Comentarios</a></li><li><a href="/users/soker/network">Amigos</a></li><li><a href="/widgets">Widgets</a></li><li><a href="/users/soker/year">Revisión del año</a></li><li><a href="/settings">Configuración</a></li><li><a href="/logout">Salir</a></li>')
 
-$('#user-menu ul').html('<li><a href="/dashboard">Inicio</a></li><li><a href="/users/soker">Perfil</a></li><li><a href="/users/soker/history">Historial</a></li><li><a href="/users/soker/progress">Progreso</a></li><li><a href="/users/soker/collection">Colección</a></li><li><a href="/users/soker/ratings">Calificaciones</a></li><li><a href="/users/soker/lists">Listas</a></li><li><a href="/users/soker/comments">Comentarios</a></li><li><a href="/users/soker/network">Amigos</a></li><li><a href="/widgets">Widgets</a></li><li><a href="/users/soker/year">Año en números</a></li><li><a href="/settings">Configuración</a></li><li><a href="/logout">Salir</a></li>')
+$('#links-wrapper div a:first').text('Perfil')
+$('#links-wrapper div a:nth-child(2)').text('Historial')
+$('#links-wrapper div a:nth-child(3)').text('Progreso')
+$('#links-wrapper div a:nth-child(4)').text('Colección')
+$('#links-wrapper div a:nth-child(5)').text('Calificaciónes')
+$('#links-wrapper div a:nth-child(6)').text('Listas')
+$('#links-wrapper div a:nth-child(7)').text('Comentarios')
+$('#links-wrapper div a:nth-child(8)').text('Amigos')
+$('#links-wrapper div a:nth-child(9)').text('Revisión del año')
+
+//Dashboard
+if(document.title.indexOf("Dashboard") > -1) 
+{ 
+  document.title = "Inicio - Trakt.tv"; 
+  var text = $('#results-top-wrapper div:nth-child(2) div:first H1').html().replace('Hello','Hola')
+  $('#results-top-wrapper div:nth-child(2) div:first H1').html(text)
+  var text = $('#results-top-wrapper div:nth-child(2) div:first h2').html().replace('Member since','Miembro desde')
+  $('#results-top-wrapper div:nth-child(2) div:first h2').html(text)
+  $('#results-top-wrapper div:nth-child(2) div:nth-child(3)').html('Año en<br>Datos')
+  $('#results-top-wrapper div:nth-child(2) a:nth-child(2) div:first div:first').html('Tu<br>Perfil')
+}
+
+//Progress
+if(document.title.indexOf("progress") < -1) { 
+  var text = $('#progress-wrapper div:first').html();
+  text = text.replace(/episode/g,'episodio');
+  $('#progress-wrapper div:first').html(text);
+  
+  
+}
+
 
